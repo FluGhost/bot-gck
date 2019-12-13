398112cbb9bb59ec9a08790f6180ddbb35bc7c7e
FluGhost <fluvio.371@gmail.com>
2019-12-13 22:07:46 +0100

    alpha 1.0.1

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const json = require("./file.json");
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setPresence({
-    activity: { name: `prefix : c#` },
+    activity: { name: `type ${json.prefix}help for help.` },
     status: `online`
   });
 });
@@ -15,14 +15,31 @@ client.on("message", msg => {
   const pingEmbed = new MessageEmbed()
     .setTitle("Pong !")
     .setDescription(`Took : ${msg.createdTimestamp - Date.now()} ms.`)
-    .setColor("#436d44")
+    .setColor(json.colorEmbed)
     .setFooter(
       "Bot created by : FluGhost#7007",
       "https://image.noelshack.com/fichiers/2019/50/5/1576262895-flughostlogo.png"
     );
 
-  if (msg.content === "c#ping") {
+  if (msg.content === json.prefix + "ping") {
     msg.channel.send(pingEmbed);
+  }
+  /* help */
+  const helpEmbed = new MessageEmbed()
+    .setTitle("Hey ! This is help for you !")
+    .setDescription("To use a command, please use the prefix + the command.")
+    .addField("Exemple :", `${json.prefix}help`)
+    .addField("__**Prefix**__ :", `${json.prefix}`)
+    .addField("\u200b", "**Commands :**", false)
+    .addField(`help`, `This is the help message.`)
+    .addField(`ping`, `This will give you the ping of the bot.`)
+    .setColor("#436d44")
+    .setFooter(
+      "Bot created by : FluGhost#7007",
+      "https://image.noelshack.com/fichiers/2019/50/5/1576262895-flughostlogo.png"
+    );
+  if (msg.content === json.prefix + "help") {
+    msg.channel.send(helpEmbed);
   }
 });
 
