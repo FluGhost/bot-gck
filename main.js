069ce9067bb7e7d07c7a303b6954f7b9ac8c6cf0
FluGhost <fluvio.371@gmail.com>
2019-12-13 20:49:11 +0100

    0.1.0

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-
-const json = require("file.json");
+const { Discord, MessageEmbed, Client } = require("discord.js");
+const client = new Client();
+const json = require("./file.json");
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({
+    activity: { name: `prefix : c#` },
+    status: `online`
+  });
 });
 
 client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!");
+  /* ping */
+  const pingEmbed = new MessageEmbed()
+    .setTitle("Pong !")
+    .setDescription(`Took : ${msg.createdTimestamp - Date.now()} ms.`)
+    .setColor("#436d44")
+    .setFooter(
+      "Bot created by : FluGhost#7007",
+      "https://image.noelshack.com/fichiers/2019/50/5/1576262895-flughostlogo.png"
+    );
+
+  if (msg.content === "c#ping") {
+    msg.channel.send(pingEmbed);
   }
 });
 
