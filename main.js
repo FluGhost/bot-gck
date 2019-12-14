08f0a9a634c6407ee320c94ec38eb222e62e31d6
FluGhost <fluvio.371@gmail.com>
2019-12-14 15:14:47 +0100

    2.0.2

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const { Discord, MessageEmbed, Client } = require("discord.js");
 const client = new Client();
 const json = require("./file.json");
 
+/* obtenir un utilisteur depuis une mention */
+function getUserFromMention(mention) {
+  const matches = mention.match(/^<@!?(\d+)>$/);
+  if (!matches) return;
+  const id = matches[1];
+  return client.users.get(id);
+}
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
   // joue à ...
@@ -13,70 +21,53 @@ client.on("ready", () => {
 
 client.on("message", msg => {
   if (msg.author.bot) return;
-  const args = msg.content.slice(json.prefix.length).split(/ +/);
-  const cmd = args.shift().toLowerCase();
-
-  /* ping */
-
-  if (args[0] === "ping") {
-    const pingEmbed = new MessageEmbed()
-      .setTitle("Pong !")
-      .setDescription(`Took : ${Math.round(client.ping)} ms.`)
-      .setColor(json.colorEmbed)
-      .setFooter(
-        "Bot created by : FluGhost#7007",
-        "https://image.noelshack.com/fichiers/2019/50/5/1576262895-flughostlogo.png"
-      );
-
-    msg.channel.send(pingEmbed);
-  }
-  /* help */
-  const helpEmbed = new MessageEmbed()
-    .setTitle("Hey ! This is help for you !")
-    .setDescription("To use a command, please use the prefix + the command.")
-    .addField("Exemple :", `\`${json.prefix}help\` It's the prefix + \`help\`.`)
-    .addField("__**Prefix**__ :", `${json.prefix}`)
-    .addField("\u200b", "**Relase commands :**", false)
-    .addField(
-      `\`help\``,
-      `This is the help message. And you are on this command.`
-    )
-    .addField(`\`ping\``, `This will give you the ping of the bot.`)
-    .addField("\u200b", "**Alpha commands :**", false)
-    .addField("This commands will be crash the bot or be nothing.")
-    .addField(
-      `\`avatar\``,
-      `This will give you your avatar and you can download it.`
-    )
-    .addBlankField()
-    .setColor(json.colorEmbed)
-    .setFooter(
-      "Bot created by : FluGhost#7007",
-      "https://image.noelshack.com/fichiers/2019/50/5/1576262895-flughostlogo.png"
-    );
-  if (cmd === json.prefix + "help") {
-    msg.channel.send(helpEmbed);
-  }
-
-  /* obtenir un utilisteur depuis une mention */
-  function getUserFromMention(mention) {
-    // The id is the first and only match found by the RegEx.
-    const matches = mention.match(/^<@!?(\d+)>$/);
-
-    // If supplied variable was not a mention, matches will be null instead of an array.
-    if (!matches) return;
-
-    // However the first element in the matches array will be the entire mention, not just the ID,
-    // so use index 1.
-    const id = matches[1];
-
-    return client.users.get(id);
-  }
-
-  /* avatar */
   if (msg.content.startsWith(json.prefix)) {
-    /* donnons à l'utilisateur son avatar */
-    if (cmd === "avatar") {
+    const args = msg.content.slice(json.prefix.length).split(/ +/);
+    const cmd = args.shift().toLowerCase();
+
+    /* ping */
+    if (cmd === "ping") {
+      const pingEmbed = new MessageEmbed()
+        .setTitle("Pong !")
+        .setDescription(`Took : ${Math.round(client.ping)} ms.`)
+        .setColor(json.colorEmbed)
+        .setFooter(
+          "Bot created by : FluGhost#7007",
+          "https://image.noelshack.com/fichiers/2019/50/5/1576262895-flughostlogo.png"
+        );
+      msg.channel.send(pingEmbed);
+    } else if (cmd === "help") {
+      /* help */
+      const helpEmbed = new MessageEmbed()
+        .setTitle("Hey ! This is help for you !")
+        .setDescription(
+          "To use a command, please use the prefix + the command."
+        )
+        .addField(
+          "Exemple :",
+          `\`${json.prefix}help\` It's the prefix + \`help\`.`
+        )
+        .addField("__**Prefix**__ :", `${json.prefix}`)
+        .addField("\u200b", "**Relase commands :**", false)
+        .addField(
+          `\`help\``,
+          `This is the help message. And you are on this command.`
+        )
+        .addField(`\`ping\``, `This will give you the ping of the bot.`)
+        .addField("\u200b", "**Alpha commands :**", false)
+        .addField("This commands will be crash the bot or be nothing.")
+        .addField(
+          `\`avatar\``,
+          `This will give you your avatar and you can download it.`
+        )
+        .addBlankField()
+        .setColor(json.colorEmbed)
+        .setFooter(
+          "Bot created by : FluGhost#7007",
+          "https://image.noelshack.com/fichiers/2019/50/5/1576262895-flughostlogo.png"
+        );
+      msg.channel.send(helpEmbed);
+    } else if (cmd === "avatar") {
       //Si c'est la commande avatar
       if (!args) {
         //Si il n'y a pas d'arguments on donne l'avatar de l'auteur du message
@@ -113,7 +104,9 @@ client.on("message", msg => {
           msg.channel.send(avatarAutherEmbed);
         } else {
           //Si il n'y a pas d'ID
-          msg.reply("tu n'as pas donné une mention valable");
+          msg.reply(
+            `You don't use the corrects aguments. Please read this for more informations : \`${json.prefix}help\``
+          );
         }
       }
     }
